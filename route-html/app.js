bfec3ac91c5e88d2eff8fa8de2a891888a756449
dpillai <Deepak2Pillai@gmail.com>
2021-02-27 10:40:27 -0600

    learning basic routing through basic node

--- route-html/app.js
+++ route-html/app.js
@@ -1,11 +1,36 @@
-//let url = require('url');
+const fs = require('fs');
+
+function renderHTML(path, response) {
+        fs.readFile(path,null, function(error,data){
+            if (error) {
+                response.writeHead(404);
+                response.write('File not Found');
+            }
+            else {
+                response.writeHead(200,{'Content-Type': 'text/html'});
+                response.write(data);
+            }
+            response.end();
+        });
+}
 
 module.exports = {
     handleRequest: function(request, response){
-        response.writeHead(200,{'Content-Type': 'text/html'});
 
         const path = new URL(request.url, 'http://localhost:8000/').pathname;
-        response.write(path);
-        response.end();
+        switch (path) {
+            case '/':
+                renderHTML('./index.html', response)
+                break;
+        
+            case '/login' :
+                renderHTML('./login.html', response);
+                break;
+            
+            default:
+                response.writeHead(404);
+                response.write('File not found');
+                response.end();
+        }
     }
 }
