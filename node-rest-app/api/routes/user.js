1a85532dbc364df677a85779d645ef5aa1c3dd92
Deepak Pillai <deepakpillai@pop-os.localdomain>
2021-03-25 05:38:24 -0500

    Using jsonwebtoken to create a token on successful login

--- node-rest-app/api/routes/user.js
+++ node-rest-app/api/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -62,7 +63,22 @@ router.post('/login', (req, res, next) => {
                 return res.status(401).json({message: "Unauthorized"})
             }
             if(result) {
-                return res.status(200).json({message: "Successful"})
+                const token = jwt.sign({
+                    email: user[0].email,
+                    id: user[0]._id
+                },
+                process.env.JWT_KEY,
+                {
+                   expiresIn: "1h"
+                },
+                (err, token) => {
+                    if(token) {
+                        return res.status(200).json({
+                            message: "Successful",
+                            token: token
+                        })
+                    }
+                });
             }
         })
     })
